f28051e742cfab5d26743a7b519b99a4d081f2c8
Sierra Lopez <sierralopez1006@gmail.com>
2022-11-02 13:46:38 -0700

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const express = require('express');
 
 const app = express()
 
+app.use('/places', require('./controllers/places'))
+
+//homepage
 app.get('/', function(req, res) {
     res.send('Hello World')
 })
 
+//wildcard route
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
