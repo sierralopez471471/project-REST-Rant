cf97e129b91df9a079b07df7395aa48f4501c0d4
Sierra Lopez <sierralopez1006@gmail.com>
2022-11-04 15:14:34 -0700

    adding bootstrp grid to places indx

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
     <Def>
       <main>
         <h1>HOME</h1>
+        <div>
+          <img class="home-img" src="/images/pasta-alfredo.jpg" alt="Pasta"/>
+          <div>
+            Photo by <a href="https://unsplash.com/@eaterscollective?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Eaters Collective</a> on <a href="https://unsplash.com/s/photos/free-pasta-alfredo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+          </div>
+        </div>
         <a href="/places">
           <button className="btn-primary">Places Page</button>
         </a>
