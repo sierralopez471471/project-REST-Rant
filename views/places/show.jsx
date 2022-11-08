b76cf29ebcfb62eed45db7ee873f07f35e27c65f
Sierra Lopez <sierralopez1006@gmail.com>
2022-11-08 15:52:28 -0800

    added the edit form and functionality

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,21 +5,31 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{data.place.name}</h1>
-            <h1>Rating</h1>
-            <h2>currently unrated</h2>
-            <h1>Description</h1>
-            <h2>blahblahblah</h2>
-            <h1>Comments</h1>
-            <h2>No comments yet!</h2>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                Edit
-            </a>
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                <button type="submit" className="btn btn-danger">
-                    Delete
-                </button>
-            </form>
+            <div className="row">
+                <div className="col-sm-6">
+                    <img src={data.place.pic} />
+                </div>
+                <div className="col-sm-6">
+                    <h1>{data.place.name}</h1>
+                    <h1>Rating</h1>
+                    <h2>Currently Unrated</h2>
+                    <h1>Description</h1>
+                    <h2>Located in {data.place.city}, {data.place.state}</h2>
+                    <h2>Serves {data.place.cuisines}</h2>
+                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                        Edit
+                    </a>
+                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                        <button type="submit" className="btn btn-danger">
+                            Delete
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div>
+                <h1>Comments</h1>
+                <h2>No comments yet!</h2>
+            </div>
           </main>
         </Def>
     )
