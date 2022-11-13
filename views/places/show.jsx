0249bf4cbebd43e8f0cc4ae0078ef40b7c2a4c45
Sierra Lopez <sierralopez1006@gmail.com>
2022-11-13 12:33:08 -0800

    created helper method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,14 +8,17 @@ function show (data) {
             <div className="row">
                 <div className="col-sm-6 mt-4">
                     <img src={data.place.pic} />
+                    <h3>Located in {data.place.city}, {data.place.state}</h3>
                 </div>
                 <div className="col-sm-6 mt-4">
                     <h1>{data.place.name}</h1>
                     <h2 className="text-info">Rating</h2>
                     <h3>Currently Unrated</h3>
                     <h2 className="text-info">Description</h2>
-                    <h3>Located in {data.place.city}, {data.place.state}</h3>
-                    <h3>Serves {data.place.cuisines}</h3>
+                    <h3>
+                        {data.place.showEstablished()}
+                    </h3>
+                    <h4>Serving {data.place.cuisines}</h4>
                     <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                         Edit
                     </a>
