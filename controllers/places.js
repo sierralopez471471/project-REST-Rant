6f5b5734007cb7e4e8be1b776a0195e0f8445764
Sierra Lopez <sierralopez1006@gmail.com>
2022-11-15 21:24:40 -0800

    comment seed data with population

--- controllers/places.js
+++ controllers/places.js
@@ -32,9 +32,12 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+//SHOW
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
+    console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+          return (
+            <div className="border">
+              <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+              <h4>{c.content}</h4>
+              <h3>
+                <stong>- {c.author}</stong>
+              </h3>
+              <h4>Rating: {c.stars}</h4>
+            </div>
+          )
+        })
+    }
     return (
         <Def>
           <main>
@@ -31,7 +50,7 @@ function show (data) {
             </div>
             <div>
                 <h2 className="text-info">Comments</h2>
-                <h3>No comments yet!</h3>
+                {comments}
             </div>
           </main>
         </Def>
